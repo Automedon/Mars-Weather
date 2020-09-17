3bdef89a1d93b7f7f65d6e6024f228f570ed532b
Automedon <42299107+Automedon@users.noreply.github.com>
2020-09-17 20:16:48 +0700

    data from api nasa

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,41 @@
-// Where the cool stuff happens
+const API_KEY = "DEMO_KEY";
+const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
 
+const previousWeatherToggle = document.querySelector(".show-previous-weather");
 
-// Change this if you need to
-const previousWeatherToggle = document.querySelector('.show-previous-weather');
+const previousWeather = document.querySelector(".previous-weather");
 
-const previousWeather = document.querySelector('.previous-weather')
+const currentSolElement = document.querySelector("[data-current-sol]");
+previousWeatherToggle.addEventListener("click", () => {
+  previousWeather.classList.toggle("show-weather");
+});
+let selectSolIndex;
+getWeather().then((sols) => {
+  selectSolIndex = sols.length - 1;
+  displaySelectedSol(sols);
+});
 
-previousWeatherToggle.addEventListener('click', () => {
-    previousWeather.classList.toggle('show-weather')
-} )
+function displaySelectedSol(sols) {
+  const selectedSol = sols[selectSolIndex];
+
+  currentSolElement.innerText = selectedSol.sol;
+}
+
+function getWeather() {
+  return fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      const { sol_keys, validity_checks, ...solData } = data;
+      return Object.entries(solData).map(([sol, data]) => {
+        return {
+          sol,
+          maxTemp: data.AT.mx,
+          minTemp: data.AT.mn,
+          windSpeed: data.HWS?.av || 7.2,
+          windDirectionDegrees: data.WD?.most_common?.compass_degrees || 293,
+          windDirectionCardinal: data.WD?.most_common?.compass_point || "WNW",
+          date: new Date(data.First_UTC),
+        };
+      });
+    });
+}
